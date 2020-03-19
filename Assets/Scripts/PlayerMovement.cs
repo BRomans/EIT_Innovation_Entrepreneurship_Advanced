499dafabb2d9624083de5a5748ed36a8c51e7a3c
bromans <michele.romani.gzl0@gmail.com>
2020-03-19 11:40:22 +0100

    :sparkles: implemented path movement for VR player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Adjust the speed for the application.
+    public float speed = 1.0f;
+
+    // The target (cylinder) position.
+    public GameObject target;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Move our position a step closer to the target.
+        float step =  speed * Time.deltaTime; // calculate distance to move
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (!(Vector3.Distance(transform.position, target.transform.position) < 0.001f))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+        }
+    }
+
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void setTarget(GameObject target) {
+        this.target = target;
+    }
+}
