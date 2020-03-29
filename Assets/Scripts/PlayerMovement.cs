c36d7c1b73f1decbe07bc391f0b560085a950345
bromans <michele.romani.gzl0@gmail.com>
2020-03-29 14:46:19 +0200

    :bug: implemented buggy collision detection for hands clapping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     // Adjust the speed for the application.
     public float speed = 1.0f;
-
+    // Detect if the player has started
+    private bool isStarted = false;
     // The target (cylinder) position.
     public GameObject target;
 
@@ -17,17 +18,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // Move our position a step closer to the target.
-        float step =  speed * Time.deltaTime; // calculate distance to move
+        if(isStarted) {
+            // Move our position a step closer to the target.
+            float step =  speed * Time.deltaTime; // calculate distance to move
 
-        // Check if the position of the cube and sphere are approximately equal.
-        if (!(Vector3.Distance(transform.position, target.transform.position) < 0.001f))
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+            // Check if the position of the cube and sphere are approximately equal.
+            if (!(Vector3.Distance(transform.position, target.transform.position) < 0.001f))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
 
+            }
         }
     }
 
+    public void startMoving() {
+        this.isStarted = true;
+    }
+
+    public void stopMoving() {
+        this.isStarted = false;
+    }
 
     public void setSpeed(float speed) {
         this.speed = speed;
